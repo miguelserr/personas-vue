6b3e92eb032111a0f739b8d0169fde18b2110624
miguelserr <aristizabalmiguelangel50@gmail.com>
2024-11-19 13:24:43 -0500

    Se aplica el menu de la aplicacion

--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,38 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createWebHashHistory, createRouter } from 'vue-router';
+
+import EditarComuna from '../components/comunas/EditarComuna.vue';
+import NewComuna from '../components/comunas/NewComuna.vue';
+import Comunas from '../views/Comunas';
+import Home from '../views/Home.vue';
 
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: HomeView
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/comunas',
+    name: 'Comunas',
+    component: Comunas
+  },
+  {
+    path: '/editar-comuna/:id',
+    name: 'EditarComuna',
+    component: EditarComuna
+  },
+  {
+    path: '/add-comuna/',
+    name: 'NewComuna',
+    component: NewComuna
   },
   {
     path: '/about',
-    name: 'about',
+    name: 'About',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../views/about.vue')
   }
 ]
 
@@ -21,5 +40,3 @@ const router = createRouter({
   history: createWebHashHistory(),
   routes
 })
-
-export default router
